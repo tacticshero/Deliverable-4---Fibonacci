af9f2d8547657e535f24f5620e612748dc357e89
rosha <rosha@LAPTOP-78I17KBO>
2022-09-20 23:54:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Deliverable__4___Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define fibonacci array with first two elements
+            int[] fib = new int[] { 0, 1 };
+
+
+            int user_length = 25;
+
+            while (fib.Length < user_length)
+            {
+                int next = fib[fib.Length - 1] + fib[fib.Length - 2];
+                fib = fib.Append(next).ToArray();
+            }
+
+            for (int i = 0; i < fib.Length; i++)
+            {
+                Console.WriteLine($"Fibonacci number ({i}) = {fib[i]}");
+            }
+        }
+    }
+}
